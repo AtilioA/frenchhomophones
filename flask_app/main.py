04ba2bf7c773009c7b143a2b9b6e6b93a2b79148
AtilioA <atiliodadalto@hotmail.com>
2020-01-10 23:49:03 -0300

    Init branch for MongoDB

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,54 +1,27 @@
-import re
 import random
 
-from sqlalchemy import Table, Column, Integer, ForeignKey
-from flask import Flask, render_template, url_for   
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import relationship
+from flask import Blueprint, render_template, url_for
 
 from wiktionaryparser import WiktionaryParser
 # Initialize parser
 parser = WiktionaryParser()
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['DEBUG'] = True
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+from .extensions import mongo 
 
-db = SQLAlchemy(app)
-class Homophone(db.Model):
-    __tablename__ = "homophone"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(50), nullable=False)
-    partOfSpeech = db.Column(db.String(20), nullable=False)
-    definitions = relationship("Definition", back_populates="homophone")
-    homophone = db.Column(db.String(50))
-    audio = db.Column(db.String(100))
-    ipa = db.Column(db.String(50))
+main = Blueprint('main', __name__)
 
-    def __repr__(self):
-        return "<Word %r>" % self.word
-    
-class Definition(db.Model):
-    __tablename__ = "definition"
-    
-    id = db.Column(db.Integer, primary_key=True)
-    definition = db.Column(db.String(50), nullable=False)
-    homophoneId = db.Column(db.Integer, ForeignKey("homophone.id"))
-    homophone = relationship("Homophone", back_populates="definitions")
-    
-
-@app.route("/")                  
+@main.route('/')
 def index():
-    homophones = Homophone.query.all()
+    user_collection = mongo.db.homophones
+    # user_collection.insert({'name' : 'Cristina'})
+    # user_collection.insert({'name' : 'Derek'})
 
-    randomHomophone = random.choice(homophones)
     with open("french_homophones.txt", "r+", encoding="utf8") as f:
         words = f.readlines()
+    randomHomophone = random.choice(words).strip()
+    print(randomHomophone)
         
-    parsedHomophone = parser.fetch(randomHomophone, "french")
+    parsedHomophone = parser.fetch("faire", "french")
     print(f"The random homophone is \"{randomHomophone}\"")
     print(parsedHomophone)
 
@@ -90,13 +63,9 @@ def index():
         }
     except:
         homophone = None
+
+    print("HOMOPHONE:")
+    print(homophone)
+    user_collection.insert(homophone)
         
     return render_template("index.html", homophone=homophone)
-    
-@app.route("/about/")            
-def about():
-    return render_template("about.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
