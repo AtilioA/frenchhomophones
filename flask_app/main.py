727bcf2ec1acf13a9a7636f66255cbf696563028
AtilioA <atiliodadalto@hotmail.com>
2020-01-11 22:05:27 -0300

    Add search functionality

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 
 from wiktionaryparser import WiktionaryParser
 # Initialize parser
@@ -11,23 +11,40 @@ from .extensions import mongo
 main = Blueprint('main', __name__)
 
 
+def find_document(word):
+    user_collection = mongo.db.users
+
+    query = {"word": word}
+    doc = user_collection.find(query)
+    
+    print(doc)
+    return doc
+
+
 def findRandomDocument(user_collection):
+    """ Returns random homophone from the database """
     cursor = user_collection.aggregate([
         { "$sample": { "size": 1 } }
     ])
     return list(cursor)
 
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 def index():
+    # Connects to database collection. Creates one if it doesn't exist
     user_collection = mongo.db.homophones
 
-    homophone = findRandomDocument(user_collection)[0]
-    print(type(homophone))
-    print(homophone)
-
-    # cursor = user_collection.find({"word": "abader"})
-    # homophone = list(cursor)[0]
+    # For querying the database through the website
+    if request.method == 'POST':
+        word_query = request.form.get('word').strip()
+        cursor = user_collection.find({"word": word_query})
+        try:
+            homophone = list(cursor)[0]
+        except IndexError:  # No entry was found in the database
+            return render_template("notfound.html", word=word_query)
+    else:
+        # Placeholder. Will show random homophones in the index page
+        homophone = findRandomDocument(user_collection)[0]
 
     return render_template("index.html", homophone=homophone)
 
@@ -40,12 +57,3 @@ def find():
 @main.route("/about/")            
 def about():
     return render_template("about.html")
-
-
-def find_document(word):
-    user_collection = mongo.db.users
-
-    query = {"word": f"{word}" }
-    doc = user_collection.find(myquery)
-    
-    return "fodase"
