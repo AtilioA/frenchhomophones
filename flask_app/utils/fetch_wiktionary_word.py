3d4a0ad3e2f0d3e967eb7f89f6d084c481390353
AtilioA <atiliodadalto@hotmail.com>
2020-01-22 21:08:06 -0300

    Improve fetched word data structure

--- flask_app/utils/fetch_wiktionary_word.py
+++ flask_app/utils/fetch_wiktionary_word.py
@@ -5,55 +5,32 @@ sys.path.append(os.path.join(os.path.dirname(__file__), os.path.pardir))
 from wiktionaryparser import WiktionaryParser
 parser = WiktionaryParser()
 
-from . import populate_db as db
+# from . import populate_db as db
 
 
 def fetch_wiktionary_word(word):
     """ Extract word's information with WiktionaryParser """
 
-    parsedHomophone = parser.fetch(word.strip(), "french")
+    parsedHomophone = parser.fetch(word.strip(), "french")[0]
 
-    # Treat each information accordingly
-    try:
-        ipaHomophones = parsedHomophone[0]["pronunciations"]['text']
-        if len(ipaHomophones) > 1:  # Has IPA and homophones
-            ipa = ipaHomophones[0]
-            homophones = ipaHomophones[1]
-        else:
-            ipa = None
-    except:
-        pass
-    try:
-        homophones = ipaHomophones[0]
-        homophones = homophones[11:].split(',')
-        homophones[0] = homophones[0].strip()
-    except:
-        homophones = None
+    # TODO: Move to another function
+    for i, pOS in enumerate(parsedHomophone["definitions"]):
+        if pOS["partOfSpeech"] == "verb":
+            foundVerb = True
+            verbIndex = i
 
-    wordDefinitions = parsedHomophone[0]["definitions"][0]["text"][1:]
+        parsedHomophone["definitions"][i]["text"] = parsedHomophone["definitions"][i]["text"][0]
+        parsedHomophone["definitions"][i]["definitions"] = parsedHomophone["definitions"][i]["text"][1:]
+    if foundVerb:
+        parsedInfinitive = parser.fetch("test", "english")[0]
+        parsedHomophone["definitions"][i]["infinitive"] = {
+            "text": parsedInfinitive["definitions"][0]["text"][0],
+            "definitions": parsedInfinitive["definitions"][0]["text"][1:]
+        }
 
-    ipa = db.clean_IPA_string(ipa)
-    print(ipa)
-
-    try:
-        audio = parsedHomophone[0]["pronunciations"]['audio'][0]
-    except:
-        audio = parsedHomophone[0]["pronunciations"]['audio']
-
-    pronunciations = parsedHomophone[0]["pronunciations"]
-
-    homophone = {
-        'word': parsedHomophone[0]["definitions"][0]["text"][0],
-        'partOfSpeech': parsedHomophone[0]["definitions"][0]["partOfSpeech"],
-        'wordDefinitions': parsedHomophone[0]["definitions"][0]["text"][1:],
-        'sentenceExamples': parsedHomophone[0]["definitions"][0]["examples"],
-        'audio': audio,
-        'homophones': homophones,
-        'ipa': ipa,
-    }
-
-    return homophone
+    return parsedHomophone
+    pass
 
 
 if __name__ == "__main__":
-    print(fetch_wiktionary_word("particulariser"))
+    print(fetch_wiktionary_word("faire"))
