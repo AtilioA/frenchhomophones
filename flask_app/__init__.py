9d2a4aeceb5e1396df87beeb12c815bb3b5bf95b
AtilioA <atiliodadalto@hotmail.com>
2020-01-23 17:39:40 -0300

    Add robots.txt, remove unnecessary JavaScript, gzip files

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_compress import Compress
 
 from .extensions import mongo
 from .views import views
@@ -7,12 +8,13 @@ from .views import views
 def create_app(config_object='flask_app.settings'):
     """Creates a flask application instance and loads its config from config_object"""
 
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='static')
 
     app.config.from_object(config_object)
 
     mongo.init_app(app)
 
     app.register_blueprint(views)
+    Compress(app)
 
     return app
