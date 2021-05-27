b8a2658517dcca6fee7d7b10bf74fc0c89ef5785
AtilioA <atiliogpg@protonmail.com>
2021-05-27 00:40:34 -0300

    Begin stub to create TTS for missing pronunciations

--- flask_app/models.py
+++ flask_app/models.py
@@ -1,4 +1,8 @@
 import os
+# from gtts import gTTS
+# from playsound import playsound
+# from urllib.parse import quote_plus
+# import requests
 
 MONGO_URI = os.environ.get("MONGO_URI")
 
@@ -36,11 +40,28 @@ class HomophonesGroup:
             return None
 
         # Find any audio file from list of homophones
-        # If not available, get from Google Translate (this URL may break anytime)
+        # # If not available, get from Google Translate (this URL may break anytime)
         audio = f"https://translate.google.com.vn/translate_tts?ie=&q={self.homophonesList[0]['word']}&tl=fr-fr&client=tw-ob"
+        # audio = None
+
+        # headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_10_1) AppleWebKit/538.36 (KHTML, like Gecko) Chrome/41.0.2171.95 Safari/538.36'}
+        # response = requests.get("https://translate.google.com.vn/translate_tts?ie=&q=" + quote_plus(f"{self.homophonesList[0]['word']}&tl=fr-fr&client=tw-ob"), headers=headers)
+        # print(response)
+        # if response.status_code == 200:
+        #     print("Successful GET request to TTS endpoint!")
+        #     results = response.json()
+        #     print(results)
+
         for homophone in self.homophonesList:
+            # playsound("welcome1.mp3")
             if homophone['pronunciations']['audio']:
+                # print(homophone['pronunciations']['audio'])
                 audio = homophone['pronunciations']['audio']
+            # else:
+            #     language='fr'
+            #     myobj=gTTS(text=self.homophonesList[0]['word'],lang=language)
+            #     myobj.save(f"tmp/{self.homophonesList[0]['word']}.mp3")
+            # continue
 
         # Return the first if there are more than one
         if isinstance(audio, list):
